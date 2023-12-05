96a3e5add87c73b68f9bd2e075965150b757d011
Enejo Sunday Oche <enejosundayoche@gmail.com>
2023-12-05 15:21:07 +0100

    hello

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+for i in range(0, 9):
+    for j in range(0, 10):
+        if i == 8 and j == 9:
+            print("{}{}".format((i), (j)))
+        elif i != j and i < j:
+            print("{}{}".format((i), (j)), end = ", ")
 
-for digit1 in range(0, 10):
-    for digit2 in range(digit1 + 1, 10):
-        if digit1 == 8 and digit2 == 9:
-            print("{}{}".format(digit1, digit2))
-        else:
-            print("{}{}".format(digit1, digit2), end=", ")
