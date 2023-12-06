e4ad065ffd7ac6b216e8f22b785ad9dea77d7ad8
Enejo Sunday Oche <enejosundayoche@gmail.com>
2023-12-06 23:28:59 +0100

    uy

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10000, 10000)
-if number >= 0:
-    num = number % 10
-    if num > 5:
-        print(f"Last digit of {number} is {num} and is greater than 5")
-    if num == 0:
-        print(f"Last digit of {number} is {num} and is 0")
-    if num < 6 and num != 0:
-        print(f"Last digit of {number} is {num} and is less than 6 and not 0")
-else:
-    numbe = number * -1
-    num = numbe % 10
-    num = -1 * num
-    if num > 5:
-        print(f"Last digit of {number} is {num} and is greater than 5")
-    if num == 0:
-        print(f"Last digit of {number} is {num} and is 0")
-    if num < 6 and num != 0:
-        print(f"Last digit of {number} is {num} and is less than 6 and not 0")
+
+
+def search_replace(my_list, search, replace):
+    """
+    A function that replaces all occurrences
+    of an element by another in a new list
+    """
+    new_list = []
+    for element in my_list:
+        if element == search:
+            new_list.append(replace)
+        else:
+            new_list.append(element)
+    return new_list
